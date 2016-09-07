7390d559a4c72d2e0e9f5b4744f91f3d5c9c6588
jc <jianic@gmail.com>
2016-09-06 21:56:18 -0700

    adding path for ubuntu

--- module/start.sh
+++ module/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH="/root/.nvm/versions/node/v6.2.2/bin/:$PATH"
+export PATH="/home/ubuntu/.nvm/versions/node/v6.2.2/bin/:/root/.nvm/versions/node/v6.2.2/bin/:$PATH"
 echo "PATH=$PATH"
 echo "NPM path=$(which npm)"
 
